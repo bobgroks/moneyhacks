2ed89e09542bebe07a0c718611078cf9c215d2ac
bobgroks <bobgroks@hotmail.com>
2023-02-18 18:49:22 +0800

    added total ordering and dataclass for trade

--- models/trade.py
+++ models/trade.py
@@ -1,4 +1,18 @@
 import client
+from dataclasses import dataclass
+from functools import total_ordering
+
+@total_ordering
+@dataclass
+class Person:
+  name: str
+  age: int
+
+  def __eq__(self, other):
+    return self.age == other.age
+
+  def __lt__(self, other):
+    return self.age < other.age
 
 
 class Trade:
